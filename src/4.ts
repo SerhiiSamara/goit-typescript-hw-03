a5d3fbb6c91afb1667196108562938967524b0d1
SerhiiSamara <ndaair@gmail.com>
2023-09-13 22:27:54 +0300

    3

--- src/4.ts
+++ src/4.ts
@@ -63,4 +63,10 @@ class MyHouse extends Hous {
 	}
 }
 
-export { };
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+house.comeIn(person);
